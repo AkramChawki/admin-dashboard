fde5e484e746713099089125e05c3815e88cd364
AkramChawki <akramchawki01@gmail.com>
2022-02-19 13:36:55 +0100

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Filtericon, Searchicon } from './components/icons'
+import { Arrowicon, Eyeicon, Filtericon, Navicon, Searchicon, Storeicon } from './components/icons'
+import Statistics from './components/Statistics';
 import Sidebar from './partials/Sidebar'
 
 function SearchInput() {
@@ -8,55 +9,149 @@ function SearchInput() {
       <div className='p-3'>
         <Searchicon />
       </div>
-        <input type="text" placeholder="Search for stats" className='flex-1'/>
+      <input type="text" placeholder="Search for stats" className='flex-1' />
     </div>
   )
 }
 
-
+function CircleBar({ percent }: { percent: number }) {
+  const circumference = 30 * 2 * Math.PI;
+  return (
+    <div
+      className="flex items-center justify-center overflow-hidden rounded-full scale-150"
+    >
+      <svg className="w-20 h-20 -rotate-90">
+        <circle
+          className="text-transparent"
+          strokeWidth="5"
+          stroke="currentColor"
+          fill="transparent"
+          r="30"
+          cx="40"
+          cy="40"
+        />
+        <circle
+          className="text-black"
+          stroke-width="5"
+          stroke-dasharray={circumference}
+          stroke-dashoffset={circumference - percent / 100 * circumference}
+          strokeLinecap="round"
+          stroke="currentColor"
+          fill="transparent"
+          r="30"
+          cx="40"
+          cy="40"
+        />
+      </svg>
+      <span className="absolute text-black font-bold">{percent}%</span>
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <div className='w-full h-screen bg-white flex font-poppins'>
-        <Sidebar />
-        <div className='flex-1 overflow-y-scroll p-8 grid grid-cols-12 gap-6'>
-          <div className='col-span-9'>
-            <SearchInput />
-            <div className='flex items-center justify-between my-8'>
-              <div>
-                <h1 className='text-black font-bold text-3xl mb-1'>Hello David</h1>
-                <p className='text-sm text-gray-400'>Welcome Back!</p>
-              </div>
+      <Sidebar />
+      <div className='flex-1 overflow-y-scroll p-8 grid grid-cols-12 gap-6'>
+        <div className='col-span-9'>
+          <SearchInput />
+          <div className='flex items-center justify-between my-8'>
+            <div>
+              <h1 className='text-black font-bold text-3xl mb-1'>Hello David</h1>
+              <p className='text-sm text-gray-400'>Welcome Back!</p>
+            </div>
 
-              <button className='flex items-center px-4 py-2 border-2 space-x-3 border-gray-200 rounded-xl text-gray-400'>
-                <span>Filters</span>
-                <Filtericon />
-              </button>
+            <button className='flex items-center px-4 py-2 border-2 space-x-3 border-gray-200 rounded-xl text-gray-400'>
+              <span>Filters</span>
+              <Filtericon />
+            </button>
+
+          </div>
+          <div className="grid grid-cols-2 gap-6">
+            <div className="col-span-2 bg-[#f9f8f3] p-8 rounded-3xl grid grid-cols-3">
+              <div className='space-y-4'>
+                <div className='bg-[#e4e0d5] w-12 h-12 text-black rounded-full grid place-content-center'>
+                  <Eyeicon />
+                </div>
+                <p className='text-[#8c8c8b] text-sm font-light'>Views</p>
+                <p className='text-black font-bold text-xl'>500</p>
+                <p className='text-[#8c8c8b] text-sm font-light'>Per Day</p>
+              </div>
+              <div className='space-y-4'>
+                <div className='bg-[#e4e0d5] w-12 h-12 text-black rounded-full grid place-content-center'>
+                  <Navicon />
+                </div>
+                <p className='text-[#8c8c8b] text-sm font-light'>Visits</p>
+                <p className='text-black font-bold text-xl'>200</p>
+                <p className='text-[#8c8c8b] text-sm font-light'>Per Minutes</p>
+              </div>
+              <div className='space-y-4'>
+                <div className='bg-[#e4e0d5] w-12 h-12 text-black rounded-full grid place-content-center'>
+                  <Storeicon />
+                </div>
+                <p className='text-[#8c8c8b] text-sm font-light'>Orders</p>
+                <p className='text-black font-bold text-xl'>5100</p>
+                <p className='text-[#8c8c8b] text-sm font-light'>Per Day</p>
+              </div>
+            </div>
+            <div className="bg-[#ffeee2] p-8 rounded-3xl relative grid grid-cols-2 gap-4 items-center">
+              <div className='text-[#d58047] absolute top-0 right-0 p-5 rotate-45'>
+                <Arrowicon />
+              </div>
+              <div>
+                <h3 className='font-bold text-black text-2xl mb-1'>Sales</h3>
+                <p className='text-[#8c8c8b] text-sm mb-3'>Total Sales Today</p>
+                <p className='font-bold text-black text-3xl mb-1'>$500</p>
+              </div>
+              <div>
+                <CircleBar percent={55} />
+              </div>
 
             </div>
-              <div className="grid grid-cols-2 gap-6">
-                <div className="col-span-2 bg-[#f9f8f3] p-10 rounded-3xl">
-
-                </div>
-                <div className="bg-[#ffeee2] p-10 rounded-3xl">
-
-                </div>
-                <div className="bg-[#eefcef] p-10 rounded-3xl">
-
-                </div>
-                <div className="bg-[#e6f5fa] p-10 rounded-3xl">
-
-                </div>
-                <div className="bg-[#f4f5f9] p-10 rounded-3xl">
-
-                </div>
+            <div className="bg-[#eefcef] p-8 rounded-3xl relative grid grid-cols-2 gap-4 items-center">
+              <div className='text-[#7bcd80] absolute top-0 right-0 p-5 rotate-45'>
+                <Arrowicon />
               </div>
-          </div>
-          <div className='col-span-3 bg-black rounded-3xl'>
-
+              <div>
+                <h3 className='font-bold text-black text-2xl mb-1'>Profit</h3>
+                <p className='text-[#8c8c8b] text-sm mb-3'>Per day ration</p>
+                <p className='font-bold text-black text-3xl mb-1'>$150</p>
+              </div>
+              <div>
+                <CircleBar percent={30} />
+              </div>
+            </div>
+            <div className="bg-[#e6f5fa] p-8 rounded-3xl relative grid grid-cols-2 gap-4 items-center">
+              <div className='text-[#74c2d9] absolute top-0 right-0 p-5 -rotate-45'>
+                <Arrowicon />
+              </div>
+              <div>
+                <h3 className='font-bold text-black text-2xl mb-1'>Orders</h3>
+                <p className='text-[#8c8c8b] text-sm mb-3'>Total Order Today</p>
+                <p className='font-bold text-black text-3xl mb-1'>1000</p>
+              </div>
+              <div>
+                <CircleBar percent={80} />
+              </div>
+            </div>
+            <div className="bg-[#f4f5f9] p-8 rounded-3xl relative grid grid-cols-2 gap-4 items-center">
+              <div className='text-black absolute top-0 right-0 p-5 -rotate-45'>
+                <Arrowicon />
+              </div>
+              <div>
+                <h3 className='font-bold text-black text-2xl mb-1'>Visits</h3>
+                <p className='text-[#8c8c8b] text-sm mb-3'>Total Visit Today</p>
+                <p className='font-bold text-black text-3xl mb-1'>400</p>
+              </div>
+              <div>
+                <CircleBar percent={55} />
+              </div>
+            </div>
           </div>
         </div>
+        <Statistics />
+      </div>
     </div>
   )
 }
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -52,3 +52,24 @@ export const Filtericon = () => (
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
     </svg>
 )
+export const Eyeicon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+    </svg>
+)
+export const Navicon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 512 512" stroke="currentColor">
+      <path d="M306.7 325.1l-144.3 55.5c-20.3 7.5-38.5-11.6-31-31l55.5-144.3c3.2-8.5 9.9-15.2 18.4-18.4l144.3-55.5c19.4-7.5 38.5 10.7 31 31l-55.5 144.3c-3.2 8.5-9.9 15.2-18.4 18.4zM255.1 224c-16.8 0-32 14.3-32 32s15.2 32 32 32c18.6 0 32.9-14.3 32.9-32s-14.3-32-32.9-32zM512 256c0 141.4-114.6 256-256 256S0 397.4 0 256 114.6 0 256 0s256 114.6 256 256zM256 48C141.1 48 48 141.1 48 256s93.1 208 208 208 208-93.1 208-208S370.9 48 256 48z"></path>
+    </svg>
+)
+export const Storeicon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 576 512" stroke="currentColor">
+      <path d="M495.5 223.2c-3.9.5-7.9.8-12.1.8-26 0-49.2-11.4-65.1-29-15.9 17.6-39.1 29-65.2 29s-49.3-11.4-65.2-29c-15.9 17.6-39 29-65.2 29-26 0-49.2-11.4-65.1-29-15.9 17.6-39.1 29-65.24 29-4.06 0-8.15-.3-12.12-.8-55.32-7.4-81.495-72.6-51.91-119.4l57.33-90.67C90.76 4.979 99.87 0 109.6 0h356.8c9.7 0 18.8 4.978 23.9 13.13l57.3 90.67c29.7 46.9 3.4 112-52.1 119.4zm4.2 31.7c3.4-.5 8.3-1.3 12.3-2.3V448c0 35.3-28.7 64-64 64H128c-35.34 0-64-28.7-64-64V252.6c3.87 1 7.86 1.8 11.97 2.3h.12c5.26.7 10.74 1.1 16.27 1.1 12.44 0 24.44-1.9 35.64-5.4V384h320V250.7c11.2 3.4 23.1 5.3 35.4 5.3 5.6 0 11-.4 16.3-1.1z"></path>
+    </svg>
+)
+export const Arrowicon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+    </svg>
+)
